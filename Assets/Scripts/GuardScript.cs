ae4cb1f8b593398f19bb43a9d5eea795295a91e3
TheNameIsGav <spqr.dominus@gmail.com>
2021-10-08 19:55:22 -0500

    Lighting and Guard functionality completed. Must be added to individual levels, probably should've made prefabs of player but we can worry about that later

--- Assets/Scripts/GuardScript.cs
+++ Assets/Scripts/GuardScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GuardScript : MonoBehaviour
 {
+
+    [SerializeField]
+    private Quaternion ARotatePoint;
+    [SerializeField]
+    private Quaternion BRotatePoint;
+    private bool rotateDirection = true;
+    private float currRotation = 0;
+
+    const float rotateIncrement = .01f; //Speed of rotation
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +25,20 @@ public class GuardScript : MonoBehaviour
         
     }
 
-    void OnPlayerDetect()
+    private void FixedUpdate()
     {
-        //TODO - Functioanlity for detecting the player
-        PlayerScript.instance.PlayerDeath();
+        if (rotateDirection) { transform.rotation = Quaternion.Lerp(ARotatePoint, BRotatePoint, currRotation += rotateIncrement); }
+        else { transform.rotation = Quaternion.Lerp(BRotatePoint, ARotatePoint, currRotation += rotateIncrement); }
+        
+        if(currRotation >= 1)
+        {
+            rotateDirection = !rotateDirection;
+            currRotation = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) { PlayerScript.instance.PlayerDeath(); }
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,6 +10,8 @@ public class PlayerScript : MonoBehaviour
 
     //Privates
     private bool isLight;
+    private Light areaLight;
+    private int lightScore;
 
     // Player Things
     public SpriteRenderer avatar;
@@ -27,6 +29,8 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         avatarBody = transform.GetComponent<Rigidbody2D>();
+        areaLight = transform.GetComponent<Light>();
+        StartLightTimer();
     }
 
     private void FixedUpdate()
@@ -84,9 +88,16 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (isLight) { areaLight.intensity = 1;
+        } else { areaLight.intensity = -1; }
+    }
+
     public void PlayerDeath()
     {
         //TODO
+        Debug.Log("Player Died");
         // This comment is a test to make sure I got the branches working
     }
 
@@ -101,5 +112,25 @@ public class PlayerScript : MonoBehaviour
     /// Gets the current state of Light
     /// </summary>
     public bool GetLightState() { return isLight; }
+
+    /// <summary>
+    /// Function to handle the on and off of lights
+    /// </summary>
+    void StartLightTimer()
+    {
+        StartCoroutine(iterateTime());
+    }
+
+    IEnumerator iterateTime()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1f);
+        int count = isLight ? 5 : 10; //If we are light, then wait 5 seconds, if we are dark then wait 10 seconds
+        for (int i = 0; i < count; i++)
+        {
+            yield return wait;
+        }
+        LightSwap();
+        StartLightTimer(); //This is super gross but w/e
+    }
 
 }
